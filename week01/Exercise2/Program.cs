094e2731041b9ae71066755c85e9d13deb1898d6
ezra.casas <97122016+ezra-casas@users.noreply.github.com>
2025-05-13 21:16:47 -0700

    Finish week01

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -2,8 +2,32 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+    Console.WriteLine("What's your grade percentage?");
+    float score = float.Parse(Console.ReadLine());
+    string grade = "";
+
+    if (score >= 90){
+      grade = "A";
     }
+    if(score >= 80 && score < 90){
+      grade = "B";
+    }
+    if(score >= 70 && score < 80){
+      grade = "C";
+    }
+    if(score >= 60 && score < 70){
+      grade = "D";
+    }
+    if(score < 60){
+      grade = "F";
+    }
+
+    if (score >= 70){
+      Console.WriteLine($"You passed the class with a {grade}");
+    }
+  }
 }
