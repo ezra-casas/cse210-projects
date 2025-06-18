41183f6abda9e684b51027e62d9c11b39ede7e08
ezra.casas <97122016+ezra-casas@users.noreply.github.com>
2025-06-18 16:11:44 -0700

    Finish

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(new DateTime(2022, 11, 3), 30, 4.8),
+            new Swimming(new DateTime(2022, 11, 3), 13, 14),
+            new Cycling(new DateTime(2022, 11, 3), 24, 18.4)
+        };
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
